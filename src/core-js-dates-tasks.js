ad378f4b239e6059e002f6726d83d06572af3688
tearzday <biverdaniil@gmail.com>
2025-03-19 14:38:42 +0300

    feat: add getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,19 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const dateObj = new Date(date);
+  const dateArr = [
+    dateObj.getHours(),
+    dateObj.getMinutes(),
+    dateObj.getSeconds(),
+  ];
+
+  const result = dateArr
+    .map((item) => (item > 9 ? item : `0${item}`))
+    .join(':');
+
+  return result;
 }
 
 /**
